54de4df3274f701cf2ff52d94bb80beed352e03f
celoazevedo <meazevedo86@gmail.com>
2021-07-26 13:56:05 -0700

    complete 1st story

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
+      // the board arrays is what will dictate the number of squares!!!
       board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
     }
   }
@@ -13,6 +15,12 @@ class App extends Component{
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div id="gameboard">
+          {/* we will map the board array that is in the state object */}
+          {this.state.board.map((value, index) => {
+            return <Square value={value} key={index}/>
+          })}
+        </div>
       </>
     )
   }
